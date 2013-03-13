52225601a7c949f846531aa9d16bb36ec8367bb4
Taras Havrysh <ter4444@gmail.com>
2013-03-13 13:28:08 +0200

    initial preparation frontend branch

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :rubygems
+
+gem "middleman", "~>2.0.14"
+
+group :assets do
+  gem 'compass-rails'
+  gem 'compass-jquery-mobile-plugin'
+end
---
+++ config.rb
@@ -0,0 +1,136 @@
+### 
+# Compass
+###
+
+# Susy grids in Compass
+#require 'susy'
+
+# Bootstrap
+#require 'compass_twitter_bootstrap'
+
+# 960.gs grids in Compass
+# First: gem install compass-960-plugin
+# require 'ninesixty'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+#
+# jQery-mobile (looks buggy yet)
+# require 'compass-jquery-mobile-plugin'
+
+###
+# Haml
+###
+
+# CodeRay syntax highlighting in Haml
+# First: gem install haml-coderay
+# require 'haml-coderay'
+
+# CoffeeScript filters in Haml
+# First: gem install coffee-filter
+# require 'coffee-filter'
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+###
+# Page command
+###
+
+#active_nav = {:class => "active"}
+
+# Per-page layout changes:
+# 
+# With no layout
+# page "/path/to/file.html", :layout => false
+# 
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+# 
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+#I18n.load_path += Dir[ File.join(Dir.getwd, 'locale', '*.{rb,yml}') ]
+#
+#langs = Dir["locale/*.yml"].map { |file| File.basename(file).gsub(".yml", "") }
+#files = Dir["source/localizable/**"]
+#
+#langs.each do |lang|
+#  I18n.locale = lang
+#  files.each do |file|
+#    src = file.split("source/").last.gsub(".haml", "")
+#    page_id = File.basename(src, File.extname(src))
+#    new_page = "/" + src.gsub("localizable", lang).
+#      gsub(page_id, I18n.t("paths.#{page_id}", :default => page_id))
+#    page new_page, :proxy => src, :ignore => true do
+#      I18n.locale = lang
+#      @lang = lang
+#      @page_id = page_id
+#    end
+#  end
+#end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+
+helpers do
+  def nav_active(page)
+    @page_id == page ? {:class => "active"} : {}
+  end
+end
+
+helpers do
+  # Calculate the years for a copyright
+  def copyright_years(start_year)
+    end_year = Date.today.year
+    if start_year == end_year
+      start_year.to_s
+    else
+      start_year.to_s + '-' + end_year.to_s
+    end
+  end
+end
+
+# Change the CSS directory
+# set :css_dir, "alternative_css_directory"
+
+# Change the JS directory
+# set :js_dir, "alternative_js_directory"
+
+# Change the images directory
+# set :images_dir, "alternative_image_directory"
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+  
+  # Minify Javascript on build
+  # activate :minify_javascript
+  
+  # Enable cache buster
+  # activate :cache_buster
+  
+  # Use relative URLs
+  # activate :relative_assets
+  
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+  
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
